2ccdc641804fcb2a7e3e771e3cf06536e23f759b
zuma-code <zumar53@protonmail.com>
2025-03-20 15:28:28 +0000

    undone cors(*)

--- config/index.js
+++ config/index.js
@@ -26,8 +26,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: "*", 
-      credentials: true, 
+      origin: [FRONTEND_URL],
     })
   );
 
